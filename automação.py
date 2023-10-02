9a58afa4014d1a9f9cecbdff9c309d82822c3ad3
Lucas56lima <108679991+Lucas56lima@users.noreply.github.com>
2023-10-02 17:27:17 -0300

    Update automação.py

--- automação.py
+++ automação.py
@@ -1,31 +1,67 @@
+from tkinter import *
+from tkinter import filedialog
 import pandas as pd
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
-contatos_df = pd.read_excel('C:/Users/Usuário/Desktop/contatos.xlsx')
-contatos_df.head()
-
-
 import urllib
 
-navegador = webdriver.Chrome()
-navegador.get("https://web.whatsapp.com/")
+def envia_mensagem(diretorio,mensagem_inserida):
+    contatos_df = pd.read_excel(str(diretorio))
+    contatos_df.head()
 
-while len(navegador.find_elements(By.ID,"side")) < 1:
-    time.sleep(1)
-    
-for i, mensagem in enumerate(contatos_df['mensagem']):
-    pessoa = contatos_df.loc[i,'nome']
-    numero = contatos_df.loc[i,'numero']
-    texto = urllib.parse.quote(f"Olá {pessoa},{mensagem}")
-    link = f"https://web.whatsapp.com/send?phone={numero}&text={texto}"
-    navegador.get(link)
-    
+    navegador = webdriver.Chrome()
+    navegador.get("https://web.whatsapp.com/")
+
     while len(navegador.find_elements(By.ID,"side")) < 1:
-        time.sleep(5)
+        time.sleep(1)
+    
+    for i, valida in enumerate(contatos_df['já enviado']):
+        if valida != "v":
+            pessoa = contatos_df.loc[i,'nome']
+            numero = contatos_df.loc[i,'numero']      
+        
+            mensagem_inserida = urllib.parse.quote(f"{pessoa},{mensagem_inserida}")
+            link = f"https://web.whatsapp.com/send?phone={numero}&text={mensagem_inserida}"
+            navegador.get(link)
+        else:
+            print("Mensagens já enviadas para contatos selecionados.")
+    
+        while len(navegador.find_elements(By.ID,"side")) < 1:
+            time.sleep(5)
                                
-    navegador.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span').send_keys(Keys.ENTER)   
-    time.sleep(10)                           
+        navegador.find_element(By.XPATH,'//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span').send_keys(Keys.ENTER)   
+        time.sleep(10)         
+
+
+
+janela = Tk()
+janela.geometry('500x300')
+janela.configure(background="#FFFAFA")
+janela.title("Automação Whatsapp")
+texto = Label(janela, text="Arquivo selecionado: ",anchor='w')
+texto.place(x=10,y=30,width=150,height=20)
+# texto.grid(column=0, row=0, padx=0, pady=0)
+
+upload_arquivo = filedialog.askopenfilename()
+
+local_arquivo = Label(janela, text= upload_arquivo,anchor='w')
+local_arquivo.place(x=10,y=80,width=300,height=20)
+
+texto_mensagem = Label(janela,text='Insira uma mensagem: ',anchor='w')
+texto_mensagem.place(x=10,y=120,width=150,height=20)
+
+mensagem_inserida = Text(janela)
+mensagem_inserida.place(x=10,y=150,width=300,height=100)
+
+
+botao = Button(janela,default='normal', text="Enviar mensagem", command=lambda: envia_mensagem(upload_arquivo,mensagem_inserida))
+botao.place(x=10,y=230,width=120,height=30)
+
+
+
+janela.mainloop()
+
+                       
     
